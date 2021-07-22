9fe24200e495ac813832353e83ecf5c3a34aabd6
guriru2222 <59474273+guriru2222@users.noreply.github.com>
2021-07-22 14:32:34 +0900

    Add files via upload

---
+++ population.js
@@ -0,0 +1,238 @@
+var population_data = 
+[
+    {
+        "pref": "北海道",
+        "population": "5228885",
+        "density": "66.68"
+    },
+    {
+        "pref": "青森県",
+        "population": "1238730",
+        "density": "128.42"
+    },
+    {
+        "pref": "岩手県",
+        "population": "1211206",
+        "density": "79.29"
+    },
+    {
+        "pref": "宮城県",
+        "population": "2303487",
+        "density": "316.31"
+    },
+    {
+        "pref": "秋田県",
+        "population": "960113",
+        "density": "82.50"
+    },
+    {
+        "pref": "山形県",
+        "population": "1068696",
+        "density": "114.63"
+    },
+    {
+        "pref": "福島県",
+        "population": "1834198",
+        "density": "133.07"
+    },
+    {
+        "pref": "茨城県",
+        "population": "2868554",
+        "density": "470.46"
+    },
+    {
+        "pref": "栃木県",
+        "population": "1934016",
+        "density": "301.81"
+    },
+    {
+        "pref": "群馬県",
+        "population": "1940333",
+        "density": "304.97"
+    },
+    {
+        "pref": "埼玉県",
+        "population": "7346836",
+        "density": "1934.52"
+    },
+    {
+        "pref": "千葉県",
+        "population": "6287034",
+        "density": "1218.99"
+    },
+    {
+        "pref": "東京都",
+        "population": "14064696",
+        "density": "6410.44"
+    },
+    {
+        "pref": "神奈川県",
+        "population": "9240411",
+        "density": "3824.50"
+    },
+    {
+        "pref": "新潟県",
+        "population": "2202358",
+        "density": "175.01"
+    },
+    {
+        "pref": "富山県",
+        "population": "1035612",
+        "density": "243.81"
+    },
+    {
+        "pref": "石川県",
+        "population": "1133294",
+        "density": "270.72"
+    },
+    {
+        "pref": "福井県",
+        "population": "767433",
+        "density": "183.14"
+    },
+    {
+        "pref": "山梨県",
+        "population": "810427",
+        "density": "181.50"
+    },
+    {
+        "pref": "長野県",
+        "population": "2049683",
+        "density": "151.14"
+    },
+    {
+        "pref": "岐阜県",
+        "population": "1979781",
+        "density": "186.40"
+    },
+    {
+        "pref": "静岡県",
+        "population": "3635220",
+        "density": "467.41"
+    },
+    {
+        "pref": "愛知県",
+        "population": "7546192",
+        "density": "1458.75"
+    },
+    {
+        "pref": "三重県",
+        "population": "1771440",
+        "density": "306.77"
+    },
+    {
+        "pref": "滋賀県",
+        "population": "1414248",
+        "density": "352.03"
+    },
+    {
+        "pref": "京都府",
+        "population": "2579921",
+        "density": "559.37"
+    },
+    {
+        "pref": "大阪府",
+        "population": "8842523",
+        "density": "4"
+    },
+    {
+        "pref": "兵庫県",
+        "population": "5469184",
+        "density": "651.01"
+    },
+    {
+        "pref": "奈良県",
+        "population": "1325437",
+        "density": "359.11"
+    },
+    {
+        "pref": "和歌山県",
+        "population": "923033",
+        "density": "195.37"
+    },
+    {
+        "pref": "鳥取県",
+        "population": "553847",
+        "density": "157.92"
+    },
+    {
+        "pref": "島根県",
+        "population": "671602",
+        "density": "100.12"
+    },
+    {
+        "pref": "岡山県",
+        "population": "1889607",
+        "density": "265.61"
+    },
+    {
+        "pref": "広島県",
+        "population": "2801388",
+        "density": "330.37"
+    },
+    {
+        "pref": "山口県",
+        "population": "1342987",
+        "density": "219.71"
+    },
+    {
+        "pref": "徳島県",
+        "population": "719704",
+        "density": "173.56"
+    },
+    {
+        "pref": "香川県",
+        "population": "951049",
+        "density": "506.75"
+    },
+    {
+        "pref": "愛媛県",
+        "population": "1335694",
+        "density": "235.32"
+    },
+    {
+        "pref": "高知県",
+        "population": "692065",
+        "density": "97.42"
+    },
+    {
+        "pref": "福岡県",
+        "population": "5138891",
+        "density": "1030.56"
+    },
+    {
+        "pref": "佐賀県",
+        "population": "812013",
+        "density": "332.70"
+    },
+    {
+        "pref": "長崎県",
+        "population": "1313103",
+        "density": "317.87"
+    },
+    {
+        "pref": "熊本県",
+        "population": "1739211",
+        "density": "234.73"
+    },
+    {
+        "pref": "大分県",
+        "population": "1124597",
+        "density": "177.36"
+    },
+    {
+        "pref": "宮崎県",
+        "population": "1070213",
+        "density": "138.36"
+    },
+    {
+        "pref": "鹿児島県",
+        "population": "1589206",
+        "density": "172.98"
+    },
+    {
+        "pref": "沖縄県",
+        "population": "1468410",
+        "density": "643.31"
+    }
+]
